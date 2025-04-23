6418b18c3f4aca059b433b849e5380df08188d1b
kangsb-010313 <kangsb668@naver.com>
2025-04-24 00:47:01 +0900

    pt03 250424 00:47

--- src/com/javaex/practice/Ex11.java
+++ src/com/javaex/practice/Ex11.java
@@ -10,27 +10,23 @@ public class Ex11 {
 		
 		System.out.println("숫자를 입력하세요");
 		int num = sc.nextInt();
-		int a = 1;
+		int a = 0;
 		
-		for(int i=1; i<=num; i++) {
-			if(num % 2 ==0){
+		if(num % 2 == 0) {//짝수일 때
+			
+			for(int i=2; i<=num; i=i+2) {
+				a = a + i;
 				
-				System.out.println();
-				num++;
-			}else {
-				if(a<=num) {
-					a= a+2;
-					System.out.println(a + num);
-				}
-			}
-
-		}
-		System.out.println("결과값: " + num);
+			}System.out.println("결과값: " + a);
+		}else {
+			for (int i =1; i<=num; i=i+2) {
+				a = a + i;
+			}System.out.println("결과값: " + a);
+		}	
+		
+		
 		sc.close();
-			
-			
-		
-		
+
 		
 	}
 	
