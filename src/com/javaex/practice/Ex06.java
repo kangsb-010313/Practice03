0399c677130f9a9f973ff0e39aa966bb43ca13b4
kangsb-010313 <kangsb668@naver.com>
2025-04-23 14:39:29 +0900

    pt03 250423 14:39

--- src/com/javaex/practice/Ex06.java
+++ src/com/javaex/practice/Ex06.java
@@ -4,9 +4,14 @@ public class Ex06 {
 
 	public static void main(String[] args) {
 		
-		int num = 1;
-		
 
+		for(int i=1; i<=100; i++) {
+			
+			if(i % 5 ==0 && i % 7 == 0) {
+				System.out.println(i);
+			}
+			
+		}
 		
 		
 	}
---
+++ src/com/javaex/practice/Ex07.java
@@ -0,0 +1,37 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex07 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		//while (true){
+			
+			System.out.println("숫자를 입력하세요");
+			int num = sc.nextInt();
+			
+//			if(num <= 7) {
+				for(int i=1; i<=num; i++) {
+					for(int a=1; a<=i; a++) {
+						System.out.print(i);
+					}
+					System.out.println("");
+				}
+
+//				num++;
+//			}
+			
+//			if(num > 7) {
+//				break;
+//			}
+			
+			
+		//}//while
+		
+		sc.close();
+	}
+
+}
