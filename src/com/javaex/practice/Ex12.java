3741f4bfeeff6319e00c29381ce1edb67e3d2858
kangsb-010313 <kangsb668@naver.com>
2025-04-24 17:51:07 +0900

    pt03 250424 17:51

---
+++ src/com/javaex/practice/Ex12.java
@@ -0,0 +1,23 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex12 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("숫자를 입력하세요");
+		int num = sc.nextInt();
+		
+		int a = 1;
+		
+		for(int i=1; i<=num; i++) {
+			a = a * i;			
+		}
+		System.out.println("결과값: " + a);
+			
+		sc.close();
+	}
+}
